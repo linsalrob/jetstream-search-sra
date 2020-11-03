19b199cc314c8aa6a1e538fe17627d4f64bbca5d
linsalrob <raedwards@gmail.com>
2020-11-02 19:34:09 -0500

    search for fasta files

---
+++ search_fasta.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+export PATH=/usr/local/sratoolkit/current/bin:/usr/local/bowtie2/current:/usr/local/RAPSearch2/bin:/usr/local/bin:/usr/bin
+
+REF_BASENAME=$1
+shift
+SRA_IDS="$@"
+
+DIR=/nas/sra-data/fasta/current
+
+# ensure the wrangler filesystem is mounted
+if [ ! -e $DIR ]; then
+    echo "ERROR: Wrangler mount is not available!" 1>&2
+    exit 1
+fi
+
+{
+
+   for SRA_ID in $SRA_IDS; do
+   
+        echo
+
+        # check wrangler cache first
+        WRANGLER_LOC=$DIR/${SRA_ID:0:3}/${SRA_ID:0:6}/${SRA_ID}.fasta.gz
+        if [ -e $WRANGLER_LOC ]; then
+            SRA_SOURCE="$WRANGLER_LOC"
+            echo "Will read $SRA_ID from $WRANGLER_LOC"
+        else
+            # not found - we should log this better
+            echo "WARNING: $SRA_ID not found on Wrangler - skipping..."
+            # empty outputs so that job stageout works
+            touch $SRA_ID.bam $SRA_ID.bam.bai
+            continue 
+        fi
+    
+        bowtie2 -p 1 -f --no-unal -x $REF_BASENAME -U $SRA_SOURCE | samtools view -bS - | samtools sort - $SRA_ID
+        # bowtie2 -p 1 -f --no-unal -x $REF_BASENAME -U $SRA_SOURCE | samtools view -bS - | samtools sort -o $SRA_ID.bam -
+    
+        samtools index $SRA_ID.bam
+
+
+    done
+
+} 2>&1
+
+
